a80026f0e94d8506c8ebd4f6dc8418af72e670c8
Thunder <151688223+Mathiack@users.noreply.github.com>
2024-10-23 21:32:02 -0300

    nova versao

---
+++ Modulo 2/Hercio/Login/v1.2/src/main/java/Main/main.java
@@ -0,0 +1,13 @@
+package Main;
+
+import Tela.Inicio;
+import javax.swing.JFrame;
+
+public class main extends JFrame {
+
+    public static void main(String[] args) {
+        JFrame j = new Inicio();
+        j.setVisible(true);
+        j.setLocationRelativeTo(null);
+    }
+}
