adb7492ad153230cca3c0274d0e74a38a34d5613
Thunder <151688223+Mathiack@users.noreply.github.com>
2024-11-11 20:50:08 -0300

    testes

---
+++ Modulo 2/Hercio/Testes/src/main/java/Testes/Testes.java
@@ -0,0 +1,9 @@
+package Testes;
+
+public class Testes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.setProperty("webdriver.gecko.driver", "C:\\IEDriverServer_x64_4.14.0\\IEDriverServer.exe");
+    }
+}
