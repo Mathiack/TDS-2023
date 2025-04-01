7444c7e79892c9f4031b452542d8ba6808fb4b03
Thunder <151688223+Mathiack@users.noreply.github.com>
2025-03-31 21:55:56 -0300

    a

---
+++ Modulo 3/Hercio/js/DOM/script.js
@@ -0,0 +1,5 @@
+const div = document.getElementById("minhaDiv");
+div.textContent = "Conteúdo atualizado via getElementById!";
+
+const destaque = document.querySelector(".destaque"); // pega somente o 1º com .destaque
+destaque.innerHTML="<strong>Texto em negrito!</strong>"
