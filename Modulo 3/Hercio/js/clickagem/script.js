abc18cf2f167b676086f25ac56e2b9a3663d8513
Thunder <151688223+Mathiack@users.noreply.github.com>
2025-04-02 20:05:24 -0300

    button action & text changing

---
+++ Modulo 3/Hercio/js/clickagem/script.js
@@ -0,0 +1,20 @@
+const botao = document.getElementById("botao");
+
+// identation's by Monari14 Copyright 2025
+botao.addEventListener(
+    "click", ()=> 
+    {
+        const nome = document.getElementById("nome").value;
+        const email = document.getElementById("email").value;
+        alert("Botão clikenado");
+        const nomeC = document.getElementById("nomeC");
+        const emailC = document.getElementById("emailC");
+        
+        nomeC.textContent=`${nome}`;
+        emailC.textContent=`${email}`;
+    });
+
+/* const campoTexto = document.getElementById("nome");
+campoTexto.addEventListener("input", (event)=> { 
+    console.log("Valor atual: ", event.target.value);
+}); */
